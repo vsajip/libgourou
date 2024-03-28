0d77cf55e1b0de1e8238d12c1e4015863135485d
Grégory Soutadé <soutade@gmail.com>
2024-03-28 21:58:07 +0100

    Update version

--- include/libgourou.h
+++ include/libgourou.h
@@ -37,7 +37,7 @@
 #define ACS_SERVER              "http://adeactivate.adobe.com/adept"
 #endif
 
-#define LIBGOUROU_VERSION       "0.8.5"
+#define LIBGOUROU_VERSION       "0.8.6"
 
 namespace gourou
 {
